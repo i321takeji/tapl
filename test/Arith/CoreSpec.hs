e985a6676407636ed32585456604c4c071fbd577
bambooG130 <i.321.takeji@gmail.com>
2018-06-16 05:56:01 +0900

    Modified: Term generator

--- test/Arith/CoreSpec.hs
+++ test/Arith/CoreSpec.hs
@@ -12,43 +12,33 @@ import Test.QuickCheck
 spec :: Spec
 spec = do
   describe "small-step == big-step" $ do
-         prop "small-step == big-step" prop_ex3_5_17
+         prop "(small-step t ->* v ==> big-step t ↓↓ v) && (big-step t ↓↓ v ==> small-step t ->* v)" prop_ex3_5_17
 
 instance Arbitrary Term where
     arbitrary = sized sizedTerm
 
-sizedTerm 0         = oneof [ return TmTrue
-                            , return TmFalse
-                            , return TmZero
+sizedTerm 0         = oneof [ pure TmTrue
+                            , pure TmFalse
+                            , pure TmZero
                             ]
-sizedTerm n | n > 0 = frequency [ (1, return TmTrue)
-                                , (1, return TmFalse)
-                                , (4, liftM3 TmIf subterm subterm subterm)
-                                , (1, return TmZero)
-                                , (2, liftM TmSucc subterm)
-                                , (2, liftM TmPred subterm)
-                                , (2, liftM TmIsZero subterm)
-                                ]
-            where
-              subterm = sizedTerm (n `div` 2)
-
--- | exercise 3.5.17: t ->* v <=> t ↓↓ v
-prop_ex3_5_17 t = isval vs && isval vb ==> vs == vb
-    where
-      vs = eval t
-      vb = evalBig t
-
-sizedTerm' 0         = oneof [ pure TmTrue
-                             , pure TmFalse
-                             , pure TmZero
-                             ]
-sizedTerm' n | n > 0 = frequency [ (1, pure TmTrue)
-                                 , (1, pure TmFalse)
-                                 , (4, TmIf <$> subterm <*> subterm <*> subterm)
+sizedTerm n | n > 0 = frequency [ (1, pure TmTrue)
+                                , (1, pure TmFalse)
+                                , (4, do a <- choose (0, n-1)
+                                         b <- choose (0, n-1-a)
+                                         TmIf <$> sizedTerm a <*> sizedTerm b <*> sizedTerm (n-1-a-b))
                                  , (1, pure TmZero)
                                  , (2, TmSucc <$> subterm)
                                  , (2, TmPred <$> subterm)
                                  , (2, TmIsZero <$> subterm)
                                  ]
             where
-              subterm = sizedTerm' (n `div` 2)
+              subterm = sizedTerm (n-1)
+
+-- | exercise 3.5.17: t ->* v <=> t ↓↓ v
+prop_ex3_5_17 t = prop_ex3_5_17_forward t .&&. prop_ex3_5_17_backward t
+
+prop_ex3_5_17_forward t = let vs =  eval t
+                          in isval vs ==> vs == evalBig t
+
+prop_ex3_5_17_backward t = let vb =  evalBig t
+                           in isval vb ==> eval t == vb
